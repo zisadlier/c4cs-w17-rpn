898e16fc927dc5c3e88ad2c7784468827d4d4e0e
zsadlier <zsadlier@umich.edu>
2017-03-18 19:15:10 -0400

    Exponentiation test

--- test_rpn.py
+++ test_rpn.py
@@ -8,4 +8,7 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+	def test_exponentiation(self):
+		result = rpn.calculate('3 2 ^')
+		self.assertEqual(9, result)
 
